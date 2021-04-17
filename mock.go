7afddce66fb3ff5e8b6bb10dce8bfa6fecabe528
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2021-04-17 20:49:47 +0100

    Added salt

--- mock.go
+++ mock.go
@@ -3,12 +3,13 @@ package hasher
 import "fmt"
 
 type Mock struct {
+	salt string
 }
 
-func NewMock() *Mock {
-	return &Mock{}
+func NewMock(salt string) *Mock {
+	return &Mock{salt: salt}
 }
 
 func (m *Mock) Hash(s string) string {
-	return fmt.Sprintf("<%s>", s)
+	return fmt.Sprintf("%s%s", m.salt, s)
 }
